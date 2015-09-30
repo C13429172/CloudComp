7fdf8416de13f54a3702411956e88b89340b1921
C13429172 <C13429172@mydit.ie>
2015-09-30 14:57:09 +0000

    prints result

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -2,7 +2,9 @@ a = input("Enter a word to check\n")
 
 reverse_word = a[::-1]
 
-if(a == reverse_word)
-	return true
-else
-	return false
+if a == reverse_word:
+	result = true
+	print (result)
+else:
+	result = false
+	print (result)
