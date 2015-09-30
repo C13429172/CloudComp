692205e416cbdd971df2a4be5d0f207c0e9ccfc1
C13429172 <C13429172@mydit.ie>
2015-09-30 15:03:54 +0000

    commented code

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -1,10 +1,15 @@
+#asking the user to enter a word
 a = input("Enter a word to check\n")
 
+#[::-1] stores the reverse of
+# a string into a new string
 reverse_word = a[::-1]
 
+#if statement to check if the 
+#word is a palendrome
 if a == reverse_word:
-	result = true
+	result = True
 	print (result)
 else:
-	result = false
+	result = False
 	print (result)
